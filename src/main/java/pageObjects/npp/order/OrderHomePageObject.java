20dc8ddea727de19f6dcb60f6251cc2bf7f35961
hhoangle <le.h.hoang11@gmail.com>
2023-05-08 11:47:06 +0700

    update code

--- src/main/java/pageObjects/npp/order/OrderHomePageObject.java
+++ src/main/java/pageObjects/npp/order/OrderHomePageObject.java
@@ -2,6 +2,7 @@ package pageObjects.npp.order;
 
 import commons.BasePage;
 import org.openqa.selenium.WebDriver;
+import pageUIs.npp.NppCategoryPageUI;
 import pageUIs.npp.NppHomePageUI;
 
 public class OrderHomePageObject extends BasePage {
@@ -11,8 +12,8 @@ public class OrderHomePageObject extends BasePage {
         this.driver = driver;
     }
     public CreateOrderPageObject createOrderButton() {
-        waitForElementVisible(driver, NppHomePageUI.CREATE_ORDER_BUTTON);
-        clickToElement(driver, NppHomePageUI.CREATE_ORDER_BUTTON);
+        waitForElementVisible(driver, NppCategoryPageUI.VIEW_PRODUCT_BUTTON);
+        clickToElement(driver, NppCategoryPageUI.VIEW_PRODUCT_BUTTON);
         return new CreateOrderPageObject(driver);
     }
 
--- src/main/java/pageUIs/npp/NppCategoryPageUI.java
+++ src/main/java/pageUIs/npp/NppCategoryPageUI.java
@@ -6,5 +6,7 @@ public class NppCategoryPageUI {
     public static final String INPUT_NEW_CATEGORY_FIELD = "//input[@placeholder='Nhập tên danh mục']";
     public static final String CANCEL_CREATE_NEW_CATEGORY_BUTTON = "//span[text()='Huỷ bỏ']";
     public static final String CONFIRM_CREATE_NEW_CATEGORY_BUTTON = "//span[text()='Xác nhận']";
-    public static final String VIEW_CATEGORY_BUTTON = "";
+    public static final String VIEW_CATEGORY_BUTTON = "//div[@class='title__label f aic jcc pv05 ph075 p5 gray-5 w100']";
+    public static final String VIEW_PRODUCT_BUTTON = "//div[@class='title__label f aic jcc pv05 ph075 p5 gray-5 active color-active--default w100']";
+    
 }
